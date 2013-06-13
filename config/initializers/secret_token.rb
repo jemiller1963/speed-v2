b9249ced7b8f374f8e2f8064cdf90c62912f3ab9
jemiller <jemiller1963@gmail.com>
2013-06-12 21:34:02 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpeedV2::Application.config.secret_token = '7206d85f1b83e13fa7c73e8052aaa11054b481cee7865e10e6f80ec526794d0aad1589f4829cd0ff348d0d5dcdab334b6fa2795f64347ec8dd8007d49be2e225'
